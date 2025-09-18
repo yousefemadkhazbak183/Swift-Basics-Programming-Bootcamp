92a32e305ada15fcf1939d90d51e206926db3bee
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-09-18 07:25:31 +0300

    - Difference between var & let.

---
+++ SwiftBasicsBootcamp.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+ 
+// Difference between var , let
+
+import Foundation
+
+// Constant
+let someConst: Bool = false
+
+//someConst = true
+
+// Variables
+var someVariable: Bool = true
+
+someVariable = false
+
+
