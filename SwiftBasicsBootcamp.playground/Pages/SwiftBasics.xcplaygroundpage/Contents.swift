9f9820233f377ff980ed30c2537f0ce4454da829
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-09-18 03:38:54 +0300

    - First Commit in Swift programming language.

---
+++ SwiftBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+
+// This is the first my program in Swift programming lanugage.
+
+import Foundation
+
+var greeting = "Hello, playground";
+
+print(greeting)
+
+let content = "This is The First My Code in Swift Language"
+
+print(content)
+
+
+let firstGreeting = "Hello, World!"
+
+print(firstGreeting)
